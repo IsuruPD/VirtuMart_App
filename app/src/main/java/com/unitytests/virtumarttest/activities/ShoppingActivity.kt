2b509ee20d3e93ee6fb52d3b087cfbd72e2801b5
Isuru Dissanayake <56559893+IsuruPD@users.noreply.github.com>
2024-03-05 15:19:55 +0530

    Bottom Navigation Bar

--- app/src/main/java/com/unitytests/virtumarttest/activities/ShoppingActivity.kt
+++ app/src/main/java/com/unitytests/virtumarttest/activities/ShoppingActivity.kt
@@ -2,11 +2,21 @@ package com.unitytests.virtumarttest.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.unitytests.virtumarttest.R
+import com.unitytests.virtumarttest.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
+
+    val binding by lazy{
+        ActivityShoppingBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.shoppingHostFragment)
+        binding.bottonNavBar.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/unitytests/virtumarttest/fragments/shopping/HomeFragment.kt
@@ -0,0 +1,8 @@
+package com.unitytests.virtumarttest.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.unitytests.virtumarttest.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+
+}
